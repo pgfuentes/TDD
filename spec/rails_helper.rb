37f36ad32b3ab3ab18d16d367d6a5d938152ec14
Pierre <pgfuentes@yahoo.com>
2019-10-29 23:26:01 +0000

    connecting grams with users

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,10 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+
+ # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
